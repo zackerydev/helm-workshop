90936e66f7d7b178d4284a84b1a40da1ad85799b
Zackery Griesinger <zgriesinger@gmail.com>
2023-05-19 17:46:54 -0500

    Fix ci

--- setup.zsh
+++ setup.zsh
@@ -1,21 +1,21 @@
 # Setup Local Kubernetes Cluster and Registry
 
 echo -n "🛠️ Installing Dependencies...\t\t\t"
-brew install --quiet kubernetes-cli helm kind tilt-dev/tap/ctlptl
+output=`brew install --quiet kubernetes-cli helm kind tilt-dev/tap/ctlptl colima docker` 2>&1 || ((echo "❌ $output" && exit 1))
 echo "✅"
 
 echo -n "🐳 Ensuring Colima is running...\t\t"
 
-colima stop > /dev/null 2>&1
+output=`colima stop 2>&1` || (echo "❌ $output" && exit 1)
 
-colima start --disk 100 > /dev/null 2>&1
+output=`colima start --disk 100  2>&1` || (echo "❌ $output" && exit 1)
 
 echo "✅"
 
 echo -n "☸️ Creating local Kubernetes cluster...\t\t"
 # Todo figure out if we can make this port somewhat stable
 #   Ignoring exit code
-ctlptl apply -f setup/cluster.yaml > /dev/null 2>&1 || true
+outout=`ctlptl apply -f setup/cluster.yaml  2>&1` || (echo "❌ $output" && exit 1)
 
 echo "✅"
 
@@ -25,13 +25,13 @@ REGISTRY=$(ctlptl get cluster kind-kind -o template --template '{{.status.localR
 
 echo -n "🐋 Building Docker images...\t\t\t"
 
-docker build -f cowsay-app/Dockerfile -t $REGISTRY/cowsay:latest cowsay-app > /dev/null 2>&1 &
+output=`docker build -f cowsay-app/Dockerfile -t $REGISTRY/cowsay:latest cowsay-app  2>&1 &` || (echo "❌ $output" && exit 1)
 
-docker build -f exercise-three/user-service/Dockerfile -t $REGISTRY/user-service:latest exercise-three/user-service > /dev/null 2>&1 &
+output=`docker build -f exercise-three/user-service/Dockerfile -t $REGISTRY/user-service:latest exercise-three/user-service  2>&1 &` || (echo "❌ $output" && exit 1)
 
-docker build -f exercise-three/download-service/Dockerfile -t $REGISTRY/download-service:latest exercise-three/download-service > /dev/null 2>&1 &
+output=`docker build -f exercise-three/download-service/Dockerfile -t $REGISTRY/download-service:latest exercise-three/download-service  2>&1 &` || (echo "❌ $output" && exit 1)
 
-docker build -f exercise-three/invoice-service/Dockerfile -t $REGISTRY/invoice-service:latest exercise-three/invoice-service > /dev/null 2>&1 &
+output=`docker build -f exercise-three/invoice-service/Dockerfile -t $REGISTRY/invoice-service:latest exercise-three/invoice-service  2>&1 &` || (echo "❌ $output" && exit 1)
 
 wait
 
@@ -39,13 +39,13 @@ echo "✅"
 
 echo -n "🌐 Pushing Docker images...\t\t\t"
 
-docker push $REGISTRY/cowsay:latest > /dev/null 2>&1 &
+output=`docker push $REGISTRY/cowsay:latest  2>&1 &` || (echo "❌ $output" && exit 1)
 
-docker push $REGISTRY/user-service:latest > /dev/null 2>&1 &
+output=`docker push $REGISTRY/user-service:latest  2>&1 &` || (echo "❌ $output" && exit 1)
 
-docker push $REGISTRY/download-service:latest > /dev/null 2>&1 &
+output=`docker push $REGISTRY/download-service:latest  2>&1 &` || (echo "❌ $output" && exit 1)
 
-docker push $REGISTRY/invoice-service:latest > /dev/null 2>&1 &
+output=`docker push $REGISTRY/invoice-service:latest  2>&1 &` || (echo "❌ $output" && exit 1)
 
 wait
 
@@ -53,28 +53,28 @@ echo "✅"
 
 echo -n "⚙️ Creating namespaces...\t\t\t"
 
-kubectl create namespace exercise-three > /dev/null 2>&1
+output=`kubectl create namespace exercise-three  2>&1` || (echo "❌ $output" && exit 1)
 
 echo "✅"
 
 echo -n "🚀 Installing services...\t\t\t"
 
-helm install cowsay exercise-two --namespace exercise-two --create-namespace > /dev/null 2>&1 &
+output=`helm install cowsay exercise-two --namespace exercise-two --create-namespace  2>&1 &` || (echo "❌ $output" && exit 1)
  
-helm install cowsay exercise-one --namespace exercise-one --create-namespace > /dev/null 2>&1 &
+output=`helm install cowsay exercise-one --namespace exercise-one --create-namespace  2>&1 &` || (echo "❌ $output" && exit 1)
 
-helm install invoice-db bitnami/postgresql --namespace exercise-three  -f exercise-three/db/values.yaml > /dev/null 2>&1 &
+output=`helm install invoice-db bitnami/postgresql --namespace exercise-three  -f exercise-three/db/values.yaml  2>&1 &` || (echo "❌ $output" && exit 1)
 
-helm install user-service exercise-three/user-service/chart --namespace exercise-three > /dev/null 2>&1 &
+output=`helm install user-service exercise-three/user-service/chart --namespace exercise-three  2>&1 &` || (echo "❌ $output" && exit 1)
 
 # Skip installing this, have people do it themselves
-# helm install invoice-service exercise-three/invoice-service/chart --namespace exercise-three > /dev/null 2>&1 &
+# helm install invoice-service exercise-three/invoice-service/chart --namespace exercise-three  2>&1 &
 
-helm install download-service exercise-three/download-service/chart --namespace exercise-three > /dev/null 2>&1 &
+output=`helm install download-service exercise-three/download-service/chart --namespace exercise-three  2>&1 &` || (echo "❌ $output" && exit 1)
 
 wait
 
-helm upgrade --install download-service exercise-three/download-service/chart --namespace exercise-three --set affinityEnabled=true --wait --timeout=4h > /dev/null 2>&1 &
+output=`helm upgrade --install download-service exercise-three/download-service/chart --namespace exercise-three --set affinityEnabled=true --wait --timeout=4h  2>&1 &` || (echo "❌ $output" && exit 1)
 
 echo "✅"
 
@@ -83,7 +83,7 @@ echo "✅"
 
 echo -n "⛩️ Installing nginx ingress controller...\t"
 
-kubectl apply -f setup/ingress.yaml > /dev/null 2>&1
+output=`kubectl apply -f setup/ingress.yaml  2>&1` || (echo "❌ $output" && exit 1)
 
 echo "✅"
 
@@ -92,11 +92,11 @@ sleep 10
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
-  --timeout=80s > /dev/null 2>&1
+  --timeout=80s  2>&1
 
 echo "✅"
 
-kubectl config current-context > /dev/null 2>&1
+output=`kubectl config current-context  2>&1` || (echo "❌ $output" && exit 1)
 
 echo ""
 echo "🎉 All done!"
